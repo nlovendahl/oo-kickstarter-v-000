62a61cb032d930bcc37b5e9ceb491fdfc07a6a2c
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-10-22 20:52:04 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,7 +8,7 @@ class Backer
     @backed_projects = []
   end
 
-  def back_project(title)
+  def back_project(project)
     project = Project.new(title)
     @backed_projects << project.title
   end
