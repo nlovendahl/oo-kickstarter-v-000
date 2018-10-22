cf5ef6580292875f1ece0d3d7f05fa55890f8481
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-10-22 21:02:34 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,20 @@
+class Project
+
+  attr_accessor :backers
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end
+
+  def backers
+    @backers
+  end
+
+end
